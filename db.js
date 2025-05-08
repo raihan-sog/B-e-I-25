23973721ab3f6f656ae473049a9a4330353d9fac
raihan-sog <raihan@pt-sog.com>
2025-05-08 13:46:35 +0700

    lolololo

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",       // ganti kalau kamu pakai user lain
-  password: "",       // kosong kalau default XAMPP
-  database: "sog_visitor"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
